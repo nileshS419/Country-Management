2d6f712cc4f60d0959da434f65e8a32a0ebcd418
nileshS419 <126087262+nileshS419@users.noreply.github.com>
2025-02-06 16:37:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import CountryList from "./components/CountryList";
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  const addCountry = () => {
+    const countryName = prompt("Enter country name:");
+    if (countryName) {
+      setCountries([...countries, { name: countryName, states: [] }]);
+    }
+  };
+
+  const editCountry = (index) => {
+    const newName = prompt("Enter new country name:");
+    if (newName) {
+      const updatedCountries = [...countries];
+      updatedCountries[index].name = newName;
+      setCountries(updatedCountries);
+    }
+  };
+
+  const deleteCountry = (index) => {
+    if (window.confirm("Are you sure you want to delete this country?")) {
+      const updatedCountries = [...countries];
+      updatedCountries.splice(index, 1);
+      setCountries(updatedCountries);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Country, State, and City Management</h1>
+      <button onClick={addCountry}>Add Country</button>
+      <CountryList countries={countries} editCountry={editCountry} deleteCountry={deleteCountry} setCountries={setCountries} />
+    </div>
+  );
+};
+
+export default App;
